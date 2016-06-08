f20005804eb82acd8e7cf0a40190971e3514dc47
MadhurToppo <mukul.toppo@gmail.com>
2016-06-07 23:51:00 -0500

    commit for chapter 6

--- src/resources/js/myApp.js
+++ src/resources/js/myApp.js
@@ -1,6 +1,81 @@
 /* use strict */
 var app = angular.module('MyApp', []);
-app.directive('walterwhite', function() {
+
+app.directive('ShieldCtrl', function ($scope) {
+    $scope.shieldNames = [];
+
+    this.addReigns = function () {
+        $scope.shieldNames.push("Roman Reigns:  The Juggernaut");
+    };
+
+    this.addRollins = function () {
+        $scope.shieldNames.push("Seth Rollins: Architect");
+    };
+
+    this.addAmbrose = function () {
+        $scope.shieldNames.push("Dean Ambrose: Lunatic Fringe");
+    };
+})
+
+.directive('theshield', function () {
+    return {
+        restrict: 'E',
+        controller: 'ShieldCtrl',
+        link: function (scope, element, attrs) {
+            element.bind('mousenter', function () {
+                console.log(scope.shieldNames);
+            })
+        }
+    }
+})
+    
+.directive('reigns', function () {
+    return {
+        require: 'theshield',
+        link: function (scope, element, attrs, ShieldCtrl) {
+            ShieldCtrl.addReigns();
+        }
+    }
+})
+
+
+
+    .directive('rollins', function () {
+        return {
+            require: 'theshield',
+            link: function (scope, element, attrs, ShieldCtrl) {
+                ShieldCtrl.addRollins();
+            }
+        }
+    })
+
+
+    .directive('ambrose', function () {
+        return {
+            require: 'theshield',
+            link: function (scope, element, attrs, ShieldCtrl) {
+                ShieldCtrl.addAmbrose();
+            }
+        }
+    })
+
+
+    .directive('interactiveBtn', function () {
+    return {
+        retrict: 'A',
+        link: function (scope, element, attrs) {
+            element.bind ('mouseenter', function () {
+                console.log(element);
+                element[0].innerText = "Rolled Over";
+            });
+            element.bind ('mouseleave', function () {
+                element[0].innerText = "Rolled Out";
+            })
+        }
+    }
+})
+
+.directive('walterwhite', function() {
     return {
         restrict: 'E',
         transclude: true,
